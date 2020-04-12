eb1f220bbfda1b722d1e042209097de10d30b47c
Tracey Wright <traceywright@Traceys-MacBook-Pro.local>
2020-04-12 15:10:25 +1000

    restructure project files

--- server/src/connection.ts
+++ server/src/connection.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 const connection = 'mongodb://localhost:27017/coronavirus';
-const connectDb = () => mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true });
+const connectDb = () => mongoose.connect(connection, {
+  useNewUrlParser: true, useUnifiedTopology: true,
+});
 
 export default connectDb;
