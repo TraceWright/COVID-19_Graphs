980bda4fe82449ffc307384ba95af62ead5e7180
Tracey Wright <traceywright@Traceys-MacBook-Pro.local>
2020-04-12 16:22:24 +1000

    d3 sample code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import FirstChart from './FirstChart';
 
 function App() {
   return (
     <div className="App">
+      <FirstChart />
     </div>
   );
 }
