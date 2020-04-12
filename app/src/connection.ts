4fca175c42cab3ec610ba67ddb089f282111e5e0
Tracey Wright <traceywright27@gmail.com>
2020-04-12 14:02:53 +1000

    fix mongoose deprecated warnings (#1)

--- app/src/connection.ts
+++ app/src/connection.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 const connection = 'mongodb://localhost:27017/coronavirus';
-const connectDb = () => mongoose.connect(connection);
+const connectDb = () => mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true });
 
 export default connectDb;
