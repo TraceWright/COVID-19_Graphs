e303c14f3ce34b47b62e890d77647d6ae6d594c7
Tracey Wright <traceywright@Traceys-MacBook-Pro.local>
2020-04-12 16:56:43 +1000

    aggregate query

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,15 @@ const app = express();
 const PORT = 8080;
 
 app.get('/results', async (req, res) => {
-  const results = await Results.find().count();
+  const results = await Results.aggregate([
+    {
+      $group: {
+        _id: '$notification_date',
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { _id: 1 } },
+  ]);
   res.json(results);
 });
 
